743e4fae1dcb3b8fc284cb1f795d0c1c783d0721
Marcel Goethals <marcel.goethals@gmail.com>
2016-05-29 09:54:07 +0200

    mobile controller

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ var express = require('express');
 var app = express();
 
 var posts = [];
+var lastmoves = [];
+var move = "Wit";
+
 
 var validLetters = /[ABCDEFGH]/;
 var validNumbers = /[12345678]/;
 
 
+var timerSize = 60;
+var timer = timerSize;
+
+setInterval(function(){
+    if(posts.length>0 && timer > 0){
+        timer--;
+    }
+}, 1000);
+
 app.use(express.static('static'));
 
+//post a new move vote
 app.get('/post/:text', function (req, res) {
     var value = req.params.text.toUpperCase();
 
-
+    /*
+    //check validity
     var firstLetter = value[0];
     var firstNumber = value[1];
     var secondLetter = value[3];
@@ -38,30 +52,91 @@ app.get('/post/:text', function (req, res) {
     }
 
     value = firstLetter + firstNumber + " " + secondLetter + secondNumber;
-
+    */
+    //if existing move add to count
+    var update = false;
     for(var i in posts){
         if(posts[i].value == value){
             posts[i].count++;
-            res.send('add');
-            return;
+            update = true;
+            break;
         }
     }
 
-    posts.push({
-        count: 1,
-        value: req.params.text.toUpperCase()
+    //if non existing move add to list
+    if(update===false){
+        posts.push({
+            count: 1,
+            value: req.params.text.toUpperCase()
+        });
+    }
+
+    //sort the posts so most popular is on top
+    posts = posts.sort(function(a,b){
+        if(a.count<b.count) {
+            return 1;
+        }
+        if(a.count>b.count) {
+            return -1;
+        }
+        return 0;
     });
 
     res.send('add');
 });
 
-app.get('/list', function (req, res) {
-    res.send(JSON.stringify(posts));
+app.get('/drop/:id', function(req, res){
+    if(posts[req.params.id]){
+        posts.splice(req.params.id, 1);
+    }
+    res.json("okay");
 });
 
-app.get('/clear', function(req, res){
+//list all the moves
+app.get('/list', function (req, res) {
+    res.json({
+        posts: posts,
+        lastmoves: lastmoves,
+        move: move,
+        timer: timer,
+        timerSize: timerSize
+    });
+});
+
+
+app.get('/round', function(req, res){
+    if(posts.length>0){
+        //add to lastmoves
+        lastmoves.push({
+            move: move,
+            post: posts[0]
+        });
+
+        //clear votes list
+        posts = [];
+
+        //switch move
+        if(move==="Wit") {
+            move="Zwart";
+        } else {
+            move="Wit";
+        }
+
+        //reset timer
+        timer = timerSize;
+    }
+    res.json('clear');
+});
+
+app.get('/restart', function(req, res){
+    move = "Wit";
+    lastmoves = [];
     posts = [];
-    res.send('clear');
+});
+
+app.get('/timersize/:size', function(req, res){
+    timerSize = parseInt(req.params.size);
+    timer = timerSize;
 });
 
 app.listen(3000, function () {
--- static/js/main.js
+++ static/js/main.js
@@ -1,87 +1,50 @@
 var chess = (function(){
 
-    var turnlength = 60;
     var requests = m.prop([]);
-    var timer = m.prop(turnlength);
-    var go = m.prop("Wit");
-    var lastmoves = m.prop([]);
 
     function getRequests(){
         return requests;
     }
 
-    function getTimer(){
-        return timer;
-    }
-
-    function getGo(){
-        return go;
-    }
-
-    function getLastMove(){
-        return lastmoves;
-    }
-
     window.setInterval(function(){
-        m.request({method: "GET", url: "/list"}).then(
-            function(array){
-                return array.sort(function(a,b){
-                    if(a.count<b.count) {
-                        return 1;
-                    }
-                    if(a.count>b.count) {
-                        return -1;
-                    }
-                    return 0;
-                });
-            }
-        ).then(function(array){
-            if(array.length > 0){
+        m.request({method: "GET", url: "/list"}).then(requests);
+        /*
+        then(function(array){
+            if(array.posts.length > 0){
                 timer(timer()-1);
-                if(timer() == 0){
-                    lastmoves([go()+": "+requests()[0].value].concat(lastmoves()));
+                if(timer() === 0){
                     timer(turnlength);
-                    if(go()==="Wit"){
-                        go("Zwart");
-                    } else {
-                        go("Wit");
-                    }
-
-                    m.request({method: "GET", url: "/clear"});
+                    m.request({method: "GET", url: "/round"});
                 }
             }
             return array;
-        }).then(requests);
+        })
+        */
     }, 1000);
 
     return {
-        getRequests: getRequests,
-        getTimer: getTimer,
-        getGo: getGo,
-        getLastMove: getLastMove
+        getRequests: getRequests
     };
 })();
 
 var RequestList = {
     controller: function(){
         this.requests = chess.getRequests();
-        this.timer = chess.getTimer();
-        this.go = chess.getGo();
-        this.lastmoves = chess.getLastMove();
     },
     view: function(ctrl){
+        console.log(ctrl.requests());
         return m("div", [
-            m("h4", "zet: "+ctrl.go()),
-            m("div", (ctrl.timer()<60) ? "volgende zet in: "+ctrl.timer() : "stuur jouw zet!"),
+            m("h1", {class: 'beurt'},ctrl.requests().move +" "+ ctrl.requests().timer+"s"),
+            //m("div", (ctrl.requests().timer<60) ? "Nog " + ctrl.requests().timer + " seconden" : "stuur jouw zet!"),
 
             m("h2", "Laatste zetten: "),
-            m("div", ctrl.lastmoves().map(function(move){
-                return m("div", {class:"request"}, m("span", {class:"value"}, move));
+            m("div", ctrl.requests().lastmoves.reverse().slice(0,5).map(function(move){
+                return m("div", {class:"request"}, m("span", {class:"value"}, move.move+": "+move.post.value));
             })),
 
-            m("h2", "Huidige zet: "),
+            m("h2", "Volgende zet: "),
 
-            ctrl.requests().map(function(data){
+            ctrl.requests().posts.map(function(data){
                 return m("div", {class:"request"},[
                     m("span", {class:"value"}, data.value),
                     m("span", {class:"count"}, data.count)
